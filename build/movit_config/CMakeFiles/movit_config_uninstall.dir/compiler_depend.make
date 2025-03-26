1f7d1924267c4acf545ae9b067d061d37bfad1b7
darsh <darshmenon02@gmail.com>
2025-03-26 14:59:11 +0000

    added planning scene and move group

---
+++ build/movit_config/CMakeFiles/movit_config_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for movit_config_uninstall.
+# This may be replaced when dependencies are built.
---
+++ build/movit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/darsh/ros2_ws/src/UR3_ROS2_PICK_AND_PLACE/movit_config
+# Build directory: /home/darsh/ros2_ws/src/UR3_ROS2_PICK_AND_PLACE/build/movit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
