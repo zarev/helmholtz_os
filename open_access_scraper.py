5594b7b3f747fcd9c21160a6af6b91de3cd02b56
Urvi <98593572+Urvi1206@users.noreply.github.com>
2025-11-07 13:47:19 +0000

    getting the HTML layout of the target webpage

--- open_access_scraper.py
+++ open_access_scraper.py
@@ -1,9 +1,23 @@
 import csv
 import os
 from typing import Iterable, Dict
+from bs4 import BeautifulSoup
+
 
 import requests
 
+URL = 'https://photon-science.desy.de/facilities/petra_iii/beamlines/p05_imaging_beamline/publications_from_p05/2025/index_eng.html'
+
+# save the HTML file
+OUTPUT_FILE = 'last_page1_final.html'
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/91.0.4472.124 Safari/537.36"
+    )
+}
 # Input and output files
 OPEN_ACCESS_PAPERS_FILE = os.path.join("data", "open_access_papers.csv")
 DOWNLOAD_DIR = "papers"
@@ -41,16 +55,35 @@ def download_pdf(title, url):
         print(f"✅ {title}")
     except Exception as e:
         print(f"❌ Failed {title}: {e}")
+    
+def fetch_and_save(url, filename):
+    response = requests.get(url, headers=HEADERS)
+    response.raise_for_status()  # when there is error, the code stops 
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(response.text)
+
+    print(f"Saved the page to '{filename}'")
+
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    title = soup.title.string.strip() if soup.title else "No title found"
+    print(f"Page title is: {title}")
+    print(f"Number of <p> tags in the page: {len(soup.find_all('p'))}")
+    print(f"Number of <table> tags in the page: {len(soup.find_all('table'))}")
+
 
 def main():
-    papers = list(iter_open_access_papers())
+    # papers = list(iter_open_access_papers())
 
-    if not papers:
-        print("⚠️ No papers found in the open access CSV.")
-        return
+    #if not papers:
+    #    print("⚠️ No papers found in the open access CSV.")
+    #    return
 
-    for paper in papers:
-        download_pdf(paper["Title"], paper["PDF_URL"])
+    #for paper in papers:
+    #    download_pdf(paper["Title"], paper["PDF_URL"])
+    
+    fetch_and_save(URL, OUTPUT_FILE)
 
 if __name__ == "__main__":
     main()
